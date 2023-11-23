930dada06bac8fc7f233e1229f670ef323891287
Guilherme Klein Kern <gkkern@inf.ufrgs.br>
2023-11-23 12:46:35 -0300

    Move error codes to separate header

--- src/config.h
+++ src/config.h
@@ -3,12 +3,4 @@
 
 #define MAX_SESSIONS 2
 
-#define ERR_INVALID_HOST  1
-#define ERR_SOCK_OPEN     2
-#define ERR_SOCK_CONN     3
-#define ERR_SOCK_READ     4
-#define ERR_SOCK_WRITE    5
-#define ERR_SOCK_BIND     6
-#define ERR_SOCK_ACCEPT   7
-
 #endif
